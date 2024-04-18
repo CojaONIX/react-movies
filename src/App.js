20e639248dd730a9cec255652f32d2158345393d
CojaONIX <cojaonix@gmail.com>
2024-04-18 20:57:13 +0200

    7.8 fetch is not good

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 
 console.log(process.env.REACT_APP_OMDB_TEST);
 
-function App() {
+fetch(process.env.REACT_APP_OMDB_TEST)
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+const App = () => {
   return (
     <>
       <h1>Test</h1>
